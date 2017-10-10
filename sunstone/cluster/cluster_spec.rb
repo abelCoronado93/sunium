bb004ff3f869bfcc6f192a1f2d77b9705ec890b7
juanmont <juanmont@ucm.es>
2017-10-10 10:45:00 +0200

    Changed cluster for use names instead of ids

--- sunstone/cluster/cluster_spec.rb
+++ sunstone/cluster/cluster_spec.rb
@@ -18,17 +18,17 @@ RSpec.describe "Cluster test" do
     end
 
     it "Create cluster" do
-        hosts = ["30"]
-        vnets = ["17"]
-        ds = ["102"]
+        hosts = ["default"]
+        vnets = ["vnet1"]
+        ds = ["default"]
         @cluster.create("test1", hosts, vnets, ds)
     end
 
     it "check cluster" do
         hash_info={ 
-            hosts: ["30"],
-            vnets: ["17"],
-            ds: ["102"]
+            hosts: [],
+            vnets: ["vnet1"],
+            ds: []
         }
 
         @cluster.check("test1", hash_info)
@@ -38,13 +38,13 @@ RSpec.describe "Cluster test" do
         hash = {
             hosts:[],
             vnets:[],
-            ds:[]
+            ds:["test1"]
         }
-        @group.update("test", hash)
+        @cluster.update("test1", hash)
     end
 
-    it "Delete cluster" do
-        @cluster.delete("test1")
-    end
+    #it "Delete cluster" do
+    #    @cluster.delete("test1")
+    #end
 
 end
